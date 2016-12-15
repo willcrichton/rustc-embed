4021779c4a5b21278b5a751c98d7b85abbda2f82
Will Crichton <wcrichto@stanford.edu>
2016-12-15 12:51:15 -0800

    Added individual linking

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,13 @@
 
 extern crate rustc_driver;
 extern crate syntax;
+extern crate glob;
 
-use syntax::codemap::FileLoader;
 use std::path::{PathBuf, Path};
 use std::io;
+use std::process;
+use syntax::codemap::FileLoader;
+use glob::glob;
 
 struct CompilerInput(String);
 
@@ -15,14 +18,47 @@ impl FileLoader for CompilerInput {
     fn read_file(&self, _: &Path) -> io::Result<String> { Ok(self.0.clone()) }
 }
 
+#[allow(dead_code)]
+enum LinkType {
+    Sysroot,
+    Individually
+}
+
+static LINK_TYPE: LinkType = LinkType::Individually;
+static SYSROOT: &'static str =
+    "/Users/will/.multirust/toolchains/nightly-x86_64-apple-darwin";
+
 fn run_compiler(src: String) {
     let crate_name = "foobar";
+
+    let linker_args = match LINK_TYPE {
+        LinkType::Sysroot => {
+            format!("--sysroot {}", SYSROOT)
+        },
+        LinkType::Individually => {
+            let rustlib_path =
+                format!("{}/lib/rustlib/x86_64-apple-darwin/lib", SYSROOT);
+            let libs = vec!["std", "core"];
+            let libflags = libs.into_iter().map(|lib| {
+                let matches =
+                    glob(&format!("{}/lib{}-*", rustlib_path, lib)).expect("Invalid glob");
+                let path = matches.into_iter().next()
+                    .expect(&format!("Missing lib {}", lib))
+                    .expect("Invalid path");
+                format!("--extern {}={}", lib, path.display())})
+                .collect::<Vec<String>>()
+                .join(" ");
+            format!("{} -L {}", libflags, rustlib_path)
+        }
+    };
+
     let args: Vec<String> =
         format!(
-            "_ {} --sysroot {} --crate-type dylib --cap-lints allow",
-            crate_name,
-            "/Users/will/.multirust/toolchains/nightly-x86_64-apple-darwin")
+            "_ {} {} --crate-type dylib --cap-lints allow",
+            linker_args,
+            crate_name)
         .split(' ').map(|s| s.to_string()).collect();
+
     let result = rustc_driver::run(
         move ||
             rustc_driver::run_compiler(
@@ -30,6 +66,8 @@ fn run_compiler(src: String) {
                 &mut rustc_driver::RustcDefaultCalls,
                 Some(box CompilerInput(src)),
                 None));
+
+    process::exit(result as i32);
 }
 
 fn main() {
